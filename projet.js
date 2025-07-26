afee91ae2748d1793bd4bd8e2fba1a373427fbff
ayoub26666666 <153772960+ayoub26666666@users.noreply.github.com>
2025-07-26 12:40:10 +0100

    vrs 1.1.1.0

--- projet.js
+++ projet.js
@@ -18,3 +18,25 @@ document.querySelector('form').addEventListener('submit', function(e){
     
 });
 
+document.querySelector('form').addEventListener('reset', function() {
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
+});
+
+
+document.querySelectorAll('.service-image').forEach(card => {
+    card.addEventListener('mousemove', function(e) {
+        const rect = card.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
+        const rotateY = ((x - centerX) / centerX) * 12;
+        const rotateX = ((centerY - y) / centerY) * 8;
+        card.style.transform = `scale(1.08) perspective(600px) rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
+    });
+    card.addEventListener('mouseleave', function() {
+        card.style.transform = 'scale(1.03) perspective(600px) rotateY(0deg) rotateX(0deg)';
+    });
+});
